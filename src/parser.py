c3c892dcd6459155997c876f69fdd7ac8449c676
Thijs Vogels <thijs.vogels@epfl.ch>
2019-02-28 19:02:49 +0100

    Add parser

---
+++ src/parser.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+from argparse import ArgumentParser
+
+FILES = {
+    'a': 'a_example.txt',
+    'b': 'b_lovely_landscapes.txt',
+    'c': 'c_memorable_moments.txt',
+    'd': 'd_pet_pictures.txt',
+    'e': 'e_shiny_selfies.txt',
+}
+
+def load_dataset(dataset_letter):
+    curdir = os.path.dirname(os.path.realpath(__file__))
+    datadir = os.path.join(curdir, '..', 'data')
+    filename = os.path.join(datadir, FILES[dataset_letter])
+
+    images = []
+
+    with open(filename, 'r') as fp:
+        for i, line in enumerate(fp.readlines()):
+            if i == 0:
+                continue
+            entries = line.strip().split(' ')
+            orientation = entries[0]
+            tags = set(entries[2:])
+            images.append({
+                'orientation': orientation,
+                'tags': tags
+            })
+
+    return images
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('dataset', choices=['a', 'b', 'c', 'd', 'e'])
+    args = parser.parse_args()
+
+    dataset = load_dataset(args.dataset)
+
+    print('Length', len(dataset))
+
+
+if __name__ == '__main__':
+    main()
