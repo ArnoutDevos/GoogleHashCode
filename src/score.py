5f602e430661e227cd97265de5d9d3dbff7506a2
ArnoutDevos <arnoutdev@gmail.com>
2019-02-28 20:02:38 +0100

    Implemented scoring, and seperated out two slide function, needs testing

--- src/score.py
+++ src/score.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python3
+from parser import Image
 
-def score(slides)
+def score(slides):
+    score = 0
+
     for (slide1, slide2) in zip(slides[:-1], slides[1:]):
-        for image in slide1:
-            common_tags = slide1.intersection(slide2)
+        score += score_transition(slide1, slide2)
 
-def horizontal_score(slides):
-    for (slide1, slide2) in zip(slides[:-1], slides[1:]):
-        for image in slide1:
-            for image in slide2:
-                common_tags = slide1.intersection(slide2)
-                unique_1    = slide1.difference(slide2)
-                unique_2    = slide2.difference(slide1)
+    print('Score: {}'.format(score))
 
-                score = min([common_tags, unique_1, unique_2])
+def score_transition(slide1, slide2):
+    #print(slide1)
+    #print(slide2)
 
-def main():
+    image_1_tags = set()
+    image_2_tags = set()
 
+    for image in slide1:
+        #print("Image 1 tags: {}".format(image.tags))
+        image_1_tags = image_1_tags.union(image.tags)
+        #print("Image 1 tags unionized: {}".format(image_1_tags))
+
+    for image in slide2:
+        image_2_tags = image_2_tags.union(image.tags)
+
+    common_tags = image_1_tags.intersection(image_2_tags)
+    #print("common: {}".format(len(common_tags)))
+
+    unique_1    = image_1_tags.difference(image_2_tags)
+    #print("unique1: {}".format(len(unique_1)))
+
+    unique_2    = image_2_tags.difference(image_1_tags)
+    #print("unique2: {}".format(len(unique_2)))
+
+    return len(min([common_tags, unique_1, unique_2]))
 
 if __name__ == '__main__':
-    main()
+    # Test
+    score([
+        [Image(3, 'H', set(['monkey', 'sun']))],
+        [Image(64, 'V', set(['green', 'sun'])),Image(85, 'V', set(['purple', 'sun']))],
+    ])
