988abf7d05185169809658b96f67d327ff6915ad
Ekaterina Svikhnushina <e.svikhnushina@gmail.com>
2019-02-28 20:13:20 +0100

    adding preprocessing

---
+++ src/preprocessing.py
@@ -0,0 +1,20 @@
+from parser import load_dataset
+
+
+def preprocess(dataset_letter):
+    images = parser.load_dataset(dataset_letter)
+    slides = []
+    vert_slide = []
+    for image in images:
+        if image.orientation == 'H':
+            slides.append([image])
+        else:
+            vert_slide.append(image)
+            if len(vert_slide) == 2:
+                slides.append(vert_slide)
+                vert_slide = []
+    return slides
+
+
+if __name__ == '__main__':
+    preprocess('a')
